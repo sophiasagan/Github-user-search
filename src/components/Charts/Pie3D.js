1736a0ff9d8dc891f9c37b6d03872bf73838334f
Sophia Braddock <sophiabraddock82@gmail.com>
2020-11-30 22:20:27 -0500

    doughnut chart

--- src/components/Charts/Pie3D.js
+++ src/components/Charts/Pie3D.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { Component } from "react";
+import FusionCharts from "fusioncharts";
+import Charts from "fusioncharts/fusioncharts.charts";
+import ReactFC from "react-fusioncharts";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
 
-const Pie3D = () => {
-  return <div>chart</div>;
+ReactFC.fcRoot(FusionCharts, Charts, FusionTheme);
+
+const ChartComponent = ({data}) => {
+  const chartConfigs = {
+    type: "pie3d",
+    width: '100%',
+    height: 400,
+    dataFormat: "json",
+    dataSource: {
+      chart: {
+        caption:'Languages',
+        theme:'fusion',
+        decimals: 0,
+        pieRadius: "40%",
+        // paletteColors: '#f0db4f', 
+      },
+      data,
+    },
+  };
+  return <ReactFC {...chartConfigs} />;
 };
 
-export default Pie3D;
+export default ChartComponent;
--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,9 +1,68 @@
-import React from 'react';
-import styled from 'styled-components';
-import { GithubContext } from '../context/context';
-import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
+import React from "react";
+import styled from "styled-components";
+import { GithubContext } from "../context/context";
+import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
+
 const Repos = () => {
-  return <h2>repos component</h2>;
+  const { repos } = React.useContext(GithubContext);
+  // console.log(repos);
+  let languages = repos.reduce((total, item) => {
+    // console.log(item)
+    const { language, stargazers_count } = item;
+    if (!language) return total; // removes null values from github info
+    if (!total[language]) {
+      total[language] = { label: language, value: 1, stars: stargazers_count }; // check if language is not on object create the instance
+    } else {
+      total[language] = {
+        ...total[language],
+        value: total[language].value + 1,
+        stars: total[language].stars + stargazers_count,
+      }; // keep same instance add value
+    }
+    return total;
+  }, {});
+  // console.log(languages);
+  const mostUsed = Object.values(languages)
+    .sort((a, b) => {
+      return b.value - a.value;
+    })
+    .slice(0, 7);
+  // console.log(languages)
+
+  // most stars per language
+  const mostPopular = Object.values(languages).sort((a,b)=> {
+    return b.stars - a.stars
+  }).map((item)=> {
+    return {...item, value: item.stars}
+  }).slice(0, 5)
+  console.log(mostPopular);
+
+  // example chart data
+  const chartData = [
+    {
+      label: "Html",
+      value: "290",
+    },
+    {
+      label: "JS",
+      value: "260",
+    },
+    {
+      label: "CSS",
+      value: "180",
+    },
+  ];
+
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+        {/* <ExampleChart data={chartData} /> */}
+        <Pie3D data={mostUsed} />
+        <div></div>
+        <Doughnut2D data={mostPopular} />
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
